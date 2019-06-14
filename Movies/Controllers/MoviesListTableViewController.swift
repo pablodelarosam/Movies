8546b61e947fdaf43058144ffbf6b8ca661e78ac
pablodelarosam <pablo.delarosaml@udlap.mx>
2019-06-13 23:43:35 -0500

    modify

--- Movies/Controllers/MoviesListTableViewController.swift
+++ Movies/Controllers/MoviesListTableViewController.swift
@@ -13,6 +13,15 @@ class MoviesListTableViewController: UITableViewController {
     private var cellId = "moviesCell"
     private var movies = [String : [MovieLocation]]()
     private var uniqueMovies = [String]()
+    
+    struct MoviesObjects {
+        
+        var movieName : String
+        var movieDate : [MovieLocation]
+    }
+    
+    var objectArray = [MoviesObjects]()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +44,12 @@ class MoviesListTableViewController: UITableViewController {
 //                let uniqueScores = Array(scoresSet)
           
                 self.movies = moviesLocationList
+                
+                for (key, value) in moviesLocationList {
+                    print("\(key) -> \(value)")
+                    self.objectArray.append(MoviesObjects(movieName: key, movieDate: value))
+                    
+                }
        
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
@@ -54,15 +69,14 @@ class MoviesListTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         
-        return movies.count
+        return objectArray.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? MoviesTableViewCell else { fatalError() }
-  
-//        cell.movieName.text = "Movie name: \(movies[indexPath.row].title)"
-//        cell.movieDate.text = "Movie date: \(movies[indexPath.row].created_at)"
-//    
+        cell.movieName.text = "Movie name: \(objectArray[indexPath.row].movieName)"
+       cell.movieDate.text = "Movie date: \(objectArray[indexPath.row].movieDate[0].created_at)"
+    
         return cell
     }
     
@@ -70,60 +84,20 @@ class MoviesListTableViewController: UITableViewController {
         return CGFloat(100)
         
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "movieDetail", sender: self)
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let identifier = segue.identifier {
+            switch identifier {
+            case "movieDetail":
+                let
+                break
+            default:
+                print("No segue found")
+            }
+        }
     }
-    */
-
 }
